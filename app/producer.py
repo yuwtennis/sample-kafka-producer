984d5280509fa59ea791402b2d5d24eff0c162f7
ywatanabe <yu.w.tennis@gmail.com>
2020-06-08 09:26:25 +0900

    Started coding producer

--- app/producer.py
+++ app/producer.py
@@ -2,10 +2,13 @@ from kafka-python import KafkaProducer
 
 #TODO
 #
-#Class Producer:
-#
-#
-#    def __init__(self):
-#
-#
-      
+Class Producer:
+
+    def __init__(self, **kwargs):
+   
+        self._client = KafkaProducer( bootstrap_server = kwargs['bootstrap_server'] )
+
+    # Todo
+    def send(self, topic, message):
+
+        self._client.send(topic, message)
